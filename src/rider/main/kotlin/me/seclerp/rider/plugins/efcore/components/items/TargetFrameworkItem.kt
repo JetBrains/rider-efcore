6974c1053fd23207dbfa092c36be35e8af132993
Petro Kolosov <kolosovp94@gmail.com>
2022-01-16 22:56:32 +0100

    empty line removed

--- src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/components/items/TargetFrameworkItem.kt
+++ src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/components/items/TargetFrameworkItem.kt
@@ -7,7 +7,6 @@ import javax.swing.Icon
 abstract class BaseTargetFrameworkItem(displayName: String, icon: Icon?, data: String?) :
     IconItem<String?>(displayName, icon, data)
 
-
 class TargetFrameworkItem(displayName: String, data: String) :
     BaseTargetFrameworkItem(displayName, DotnetIconResolver.resolveForType(DotnetIconType.TARGET_FRAMEWORK), data)
 
