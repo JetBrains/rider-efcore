10e6988d2ae901869d2b40a07cd002446c2948ea
Petro Kolosov <kolosovp94@gmail.com>
2022-01-27 00:43:46 +0100

    migration parent folder removed if empty

--- src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/features/migrations/remove/RemoveLastMigrationAction.kt
+++ src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/features/migrations/remove/RemoveLastMigrationAction.kt
@@ -6,6 +6,8 @@ import me.seclerp.rider.plugins.efcore.cli.api.MigrationsClient
 import me.seclerp.rider.plugins.efcore.cli.execution.executeCommandUnderProgress
 import me.seclerp.rider.plugins.efcore.cli.api.models.DotnetEfVersion
 import me.seclerp.rider.plugins.efcore.features.shared.EfCoreAction
+import me.seclerp.rider.plugins.efcore.rd.MigrationInfo
+import java.io.File
 
 class RemoveLastMigrationAction : EfCoreAction() {
     override fun ready(actionEvent: AnActionEvent, efCoreVersion: DotnetEfVersion) {
@@ -17,10 +19,36 @@ class RemoveLastMigrationAction : EfCoreAction() {
         if (dialog.showAndGet()) {
             val migrationsClient = intellijProject.getService<MigrationsClient>()
             val commonOptions = getCommonOptions(dialog)
+            val currentDbContextMigrationsList = dialog.availableMigrationsList
 
             executeCommandUnderProgress(intellijProject, "Removing migration...", "Last migration has been removed") {
-                migrationsClient.removeLast(commonOptions)
+                val res = migrationsClient.removeLast(commonOptions)
+
+                val migration = currentDbContextMigrationsList.firstOrNull()
+
+                deleteMigrationsFolderIfEmpty(migration)
+
+                res
             }
         }
+    }
+
+    private fun deleteMigrationsFolderIfEmpty(migration: MigrationInfo?) {
+        val folder = migration?.migrationFolderAbsPath
+
+        if (migration == null || folder == null) return
+
+        val folderIsEmpty = folderIsEmpty(folder)
+
+        if (folderIsEmpty) {
+            File(folder).delete()
+        }
+    }
+
+    private fun folderIsEmpty(folderPath: String): Boolean {
+        val files = File(folderPath).listFiles()
+        val isEmpty = files?.isEmpty() ?: false
+
+        return isEmpty
     }
 }
