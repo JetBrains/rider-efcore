6e30ee7ce6e67c5f1804a0c4ee2758644912f5e5
Sander Jochems <sanderjochems@hotmail.nl>
2022-07-28 16:24:13 +0200

    Update SupportedTargetFrameworks.cs

--- src/dotnet/Rider.Plugins.EfCore/Compatibility/SupportedTargetFrameworks.cs
+++ src/dotnet/Rider.Plugins.EfCore/Compatibility/SupportedTargetFrameworks.cs
@@ -6,6 +6,7 @@
         public const string Net6 = "net6.0";
         public const string Net7 = "net7.0";
         public const string NetCore31 = ".NETCoreApp,Version=v3.1";
+        public const string NetStandard20 = ".NETStandard,Version=v2.0";
         public const string NetStandard21 = ".NETStandard,Version=v2.1";
     }
 }
