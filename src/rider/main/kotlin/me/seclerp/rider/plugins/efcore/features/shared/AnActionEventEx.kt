371f1726c88df0c593814fb6583e2d6f7749a9e5
Andrew Rublyov <seclerp.dev@gmail.com>
2022-10-04 04:20:59 +0200

    Fix visibility of EF Core related actions in both menus

--- src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/features/shared/AnActionEventEx.kt
+++ src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/features/shared/AnActionEventEx.kt
@@ -1,31 +1,38 @@
 package me.seclerp.rider.plugins.efcore.features.shared
 
+import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.workspaceModel.ide.WorkspaceModel
+import com.jetbrains.rider.model.RdProjectDescriptor
+import com.jetbrains.rider.model.RdUnloadProjectDescriptor
 import com.jetbrains.rider.projectView.workspace.ProjectModelEntity
 import com.jetbrains.rider.projectView.workspace.getProjectModelEntities
-import com.jetbrains.rider.projectView.workspace.isUnloadedProject
 
 fun AnActionEvent.isEfCoreActionContext(): Boolean {
     if (project == null) return false
-    val actionFile = getData(PlatformDataKeys.VIRTUAL_FILE) ?: return true
+
+    // Case when we are presenting action from Tools application menu entry (always visible)
+    val actionFile = getData(PlatformDataKeys.VIRTUAL_FILE)
+    if (!ActionPlaces.isPopupPlace(place) || actionFile == null) {
+        return true
+    }
 
     if (!isSupportedProjectExtension(actionFile.extension ?: "")) {
         return false
     }
 
-    val fileProject = getFileProject(project!!, actionFile) ?: return true
+    getFileProject(project!!, actionFile) ?: return false
 
-    return !fileProject.isUnloadedProject()
+    return true
 }
 
-fun AnActionEvent.getDotnetProjectName(): String {
+fun AnActionEvent.getDotnetProjectName(): String? {
     val actionFile = getData(PlatformDataKeys.VIRTUAL_FILE) ?: return ""
 
-    return getFileProject(project!!, actionFile)!!.descriptor.name
+    return getFileProject(project!!, actionFile)?.descriptor?.name
 }
 
 @Suppress("UnstableApiUsage")
@@ -33,7 +40,10 @@ private fun getFileProject(intellijProject: Project, virtualFile: VirtualFile): 
     WorkspaceModel
         .getInstance(intellijProject)
         .getProjectModelEntities(virtualFile, intellijProject)
-        .firstOrNull()
+        .firstOrNull {
+            it.descriptor is RdProjectDescriptor
+            && it.descriptor !is RdUnloadProjectDescriptor
+        }
 
 private fun isSupportedProjectExtension(projectFileExtension: String) =
     projectFileExtension == "csproj"
