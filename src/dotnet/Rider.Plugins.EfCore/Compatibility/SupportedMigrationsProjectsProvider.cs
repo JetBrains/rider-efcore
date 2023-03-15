1cdf24fc9e9b30d3cfb1128af7fa2074828bdcd2
Andrii Rublov <seclerp.dev@gmail.com>
2023-03-15 02:54:05 +0100

    Fix .NET 8.0 support, bump intellij plugin version

--- src/dotnet/Rider.Plugins.EfCore/Compatibility/SupportedMigrationsProjectsProvider.cs
+++ src/dotnet/Rider.Plugins.EfCore/Compatibility/SupportedMigrationsProjectsProvider.cs
@@ -28,6 +28,7 @@ namespace Rider.Plugins.EfCore.Compatibility
       targetFrameworkId.UniqueString.StartsWith(SupportedTargetFrameworks.Net5)
       || targetFrameworkId.UniqueString.StartsWith(SupportedTargetFrameworks.Net6)
       || targetFrameworkId.UniqueString.StartsWith(SupportedTargetFrameworks.Net7)
+      || targetFrameworkId.UniqueString.StartsWith(SupportedTargetFrameworks.Net8)
       || targetFrameworkId.UniqueString.StartsWith(SupportedTargetFrameworks.NetCore31)
       || targetFrameworkId.UniqueString.StartsWith(SupportedTargetFrameworks.NetStandard20)
       || targetFrameworkId.UniqueString.StartsWith(SupportedTargetFrameworks.NetStandard21);
