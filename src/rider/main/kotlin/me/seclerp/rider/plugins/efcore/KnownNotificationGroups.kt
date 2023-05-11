1371a2c05dad2a12d886d8091cf8066b05514fa6
Andrii Rublov <andrii.rublov@jetbrains.com>
2023-05-11 23:05:10 +0200

    Fix #164

--- src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/KnownNotificationGroups.kt
+++ src/rider/main/kotlin/me/seclerp/rider/plugins/efcore/KnownNotificationGroups.kt
@@ -4,5 +4,5 @@ import org.jetbrains.annotations.NonNls
 
 object KnownNotificationGroups {
     @NonNls
-    val efCore = "EF Core Notifications Group"
+    val efCore = "EfCore.Groups.Notifications"
 }
