a6a444b64e23ddee27f134895b7ea0a6ed3683b5
denis.kuznetsov <denis.kuznetsov@jetbrains.com>
2023-09-22 18:05:50 +0200

    Fix indents for DefineRiderEfCoreConstants

--- src/dotnet/Rider.Plugins.EfCore/BuildScript/DefineRiderEfCoreConstants.cs
+++ src/dotnet/Rider.Plugins.EfCore/BuildScript/DefineRiderEfCoreConstants.cs
@@ -6,19 +6,19 @@ using JetBrains.Build;
 
 namespace Rider.Plugins.EfCore.BuildScript
 {
-	public static class DefineRiderEfCoreConstants
-	{
-		[BuildStep]
-		public static IEnumerable<AutofixAllowedDefineConstant> YieldAllowedDefineConstantsForEfCorePlugin()
-		{
-			var constants = new List<string>();
+  public static class DefineRiderEfCoreConstants
+  {
+    [BuildStep]
+    public static IEnumerable<AutofixAllowedDefineConstant> YieldAllowedDefineConstantsForEfCorePlugin()
+    {
+      var constants = new List<string>();
 
-			constants.AddRange(new[] {"$(DefineConstants)", "RIDER"});
+      constants.AddRange(new[] {"$(DefineConstants)", "RIDER"});
 
-			return constants.SelectMany(s => new []
-			{
-				new AutofixAllowedDefineConstant(new SubplatformName("Plugins\\rider-efcore\\src\\dotnet\\Rider.Plugins.EfCore"), s)
-			});
-		}
-	}
+      return constants.SelectMany(s => new []
+      {
+        new AutofixAllowedDefineConstant(new SubplatformName("Plugins\\rider-efcore\\src\\dotnet\\Rider.Plugins.EfCore"), s)
+      });
+    }
+  }
 }
