f021660efe2b7d0be3ade0f7105138d9b32002dc
Alexander Ulitin <alexander.ulitin@jetbrains.com>
2025-04-14 16:59:16 +0200

    [rider] EF Rider plugin should not be activated in ReSharper/Fleet etc
    GitOrigin-RevId: 72476534a2f0f3f4c62d2a098203598520cc127a

--- src/dotnet/Rider.Plugins.EfCore/ZoneMarker.cs
+++ src/dotnet/Rider.Plugins.EfCore/ZoneMarker.cs
@@ -1,12 +1,14 @@
 using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.Application.Environment;
 using JetBrains.Platform.RdFramework;
 using JetBrains.ProjectModel;
+using JetBrains.Rider.Backend.Env;
 
 namespace Rider.Plugins.EfCore
 {
   [ZoneMarker]
-  public class ZoneMarker
-    : IRequire<IRiderEfCoreZone>, IRequire<IProjectModelZone>, IRequire<IRdFrameworkZone>
-  {
-  }
+  public class ZoneMarker : IRequire<IRiderBackendFeatureEnvironmentZone>, IRequire<IRiderEfCoreZone>, IRequire<IProjectModelZone>, IRequire<IRdFrameworkZone>;
+
+  [ZoneActivator]
+  public class EfPluginActivator : IActivate<IRiderEfCoreZone>;
 }
