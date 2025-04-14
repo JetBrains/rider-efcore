ab651db973ab692e21db81c9ab05d7a901444fea
Alexander Ulitin <alexander.ulitin@jetbrains.com>
2025-04-14 22:40:13 +0200

    [rider] EF Rider plugin: move activator out of shell zones requirements
    GitOrigin-RevId: a5e14f5abf6f42938ced4535fb504b9ece71b60c

--- src/dotnet/Rider.Plugins.EfCore/IRiderEfCoreZone.cs
+++ src/dotnet/Rider.Plugins.EfCore/IRiderEfCoreZone.cs
@@ -1,21 +1,36 @@
 using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.Application.Environment;
+using JetBrains.Platform.RdFramework;
 using JetBrains.Platform.RdFramework.Actions.Backend;
+using JetBrains.ProjectModel;
 using JetBrains.ProjectModel.NuGet;
 using JetBrains.ProjectModel.ProjectsHost.SolutionHost;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.Rider.Backend.Env;
+using Rider.Plugins.EfCore;
 
 namespace Rider.Plugins.EfCore
 {
   [ZoneDefinition]
   // [ZoneDefinitionConfigurableFeature("Title", "Description", IsInProductSection: false)]
-  public interface IRiderEfCoreZone : IZone, IRequire<IPsiLanguageZone>,
+  public interface IRiderEfCoreZone : IZone,
+    IRequire<IPsiLanguageZone>,
     IRequire<ILanguageCSharpZone>,
     IRequire<DaemonZone>,
     IRequire<INuGetZone>,
     IRequire<IRdActionsBackendZone>,
-    IRequire<IHostSolutionZone>
+    IRequire<IHostSolutionZone>,
+    IRequire<IProjectModelZone>,
+    IRequire<IRdFrameworkZone>
   {
   }
+}
+
+namespace Rider.Plugins.EfCoreActivator
+{
+  [ZoneActivator]
+  [ZoneMarker(typeof(IRiderBackendFeatureEnvironmentZone))]
+  public class EfPluginActivator : IActivate<IRiderEfCoreZone>;
 }
--- src/dotnet/Rider.Plugins.EfCore/ZoneMarker.cs
+++ src/dotnet/Rider.Plugins.EfCore/ZoneMarker.cs
@@ -1,14 +1,8 @@
 using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.Application.Environment;
-using JetBrains.Platform.RdFramework;
-using JetBrains.ProjectModel;
-using JetBrains.Rider.Backend.Env;
 
 namespace Rider.Plugins.EfCore
 {
   [ZoneMarker]
-  public class ZoneMarker : IRequire<IRiderBackendFeatureEnvironmentZone>, IRequire<IRiderEfCoreZone>, IRequire<IProjectModelZone>, IRequire<IRdFrameworkZone>;
-
-  [ZoneActivator]
-  public class EfPluginActivator : IActivate<IRiderEfCoreZone>;
+  public class ZoneMarker : IRequire<IRiderEfCoreZone>;
 }
+
