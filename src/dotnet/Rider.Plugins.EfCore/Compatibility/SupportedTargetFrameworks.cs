9ddd037c02a8d5e5b3a67b510f1f4b57f54d1865
Arseny.Chernyaev <arseny.chernyaev@jetbrains.com>
2025-09-29 12:10:22 +0200

    Always mark new versions of .NET Core tfms as supported for EF Core migrations (fixed RIDER-130335)
    GitOrigin-RevId: ca769f3abc61b8db4466ddc6f8dc8a0989aea665

--- src/dotnet/Rider.Plugins.EfCore/Compatibility/SupportedTargetFrameworks.cs
+++ src/dotnet/Rider.Plugins.EfCore/Compatibility/SupportedTargetFrameworks.cs
@@ -1,14 +1,10 @@
-﻿namespace Rider.Plugins.EfCore.Compatibility
+﻿using System;
+
+namespace Rider.Plugins.EfCore.Compatibility
 {
   public static class SupportedTargetFrameworks
   {
-    public const string Net5 = "net5.0";
-    public const string Net6 = "net6.0";
-    public const string Net7 = "net7.0";
-    public const string Net8 = "net8.0";
-    public const string Net9 = "net9.0";
-    public const string NetCore31 = ".NETCoreApp,Version=v3.1";
-    public const string NetStandard20 = ".NETStandard,Version=v2.0";
-    public const string NetStandard21 = ".NETStandard,Version=v2.1";
+    public static readonly Version OurMinimalNetCoreSupportedVersion = new(3, 1);
+    public static readonly Version OurMinimalNetStandardSupportedVersion = new(2, 0);
   }
 }
