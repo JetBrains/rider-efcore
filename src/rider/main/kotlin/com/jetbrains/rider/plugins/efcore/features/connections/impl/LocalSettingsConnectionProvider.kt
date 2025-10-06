16c875cf2b87e346f85bcdf8be8ea81c2650755a
Ilnar Agliamzianov <ilnar.agliamzianov@jetbrains.com>
2025-10-06 18:42:32 +0300

    Rider features using VirtualFilePath and RdPath
    GitOrigin-RevId: 8092915b98174ff5a39bd38855265d0bde87b739

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/efcore/features/connections/impl/LocalSettingsConnectionProvider.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/efcore/features/connections/impl/LocalSettingsConnectionProvider.kt
@@ -3,11 +3,11 @@ package com.jetbrains.rider.plugins.efcore.features.connections.impl
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.jetbrains.rider.ijent.extensions.toNioPathOrNull
 import com.jetbrains.rider.model.RdCustomLocation
 import com.jetbrains.rider.model.RdProjectDescriptor
 import com.jetbrains.rider.plugins.efcore.features.connections.DbConnectionProvider
 import com.jetbrains.rider.plugins.efcore.features.shared.services.JsonSerializer
-import kotlin.io.path.Path
 
 /**
  * Azure Functions-specific file local.settings.json
@@ -22,7 +22,7 @@ class LocalSettingsConnectionProvider(private val intellijProject: Project) : Db
 
     override fun getAvailableConnections(project: RdProjectDescriptor) =
         buildList {
-            val directory = (project.location as? RdCustomLocation)?.customLocation?.let(::Path)?.parent ?: return@buildList
+            val directory = (project.location as? RdCustomLocation)?.customLocation?.toNioPathOrNull()?.parent ?: return@buildList
             val localSettingsFile = directory.resolve("local.settings.json").toFile()
             if (!localSettingsFile.exists() || !localSettingsFile.isFile)
                 return@buildList
