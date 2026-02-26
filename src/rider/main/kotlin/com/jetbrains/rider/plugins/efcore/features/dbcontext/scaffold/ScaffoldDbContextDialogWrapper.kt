44a708b5684597fc24233cb4aa1c7176b6d211af
Veljko Dragic <veljko.dragic@jetbrains.com>
2026-02-26 12:29:59 +0100

    RIDER-134775 rider-efcore: Fix mono repo compilation
    GitOrigin-RevId: 2f2139f4ed312a5f4093848d316a7b1be812b728
    (cherry picked from commit 20f3368c15750244fea68854b0a82e32cf064b9d)
    
    GitOrigin-RevId: b532fb2b0c7fee8a6033748e98b7ae9b225a5627

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/efcore/features/dbcontext/scaffold/ScaffoldDbContextDialogWrapper.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/efcore/features/dbcontext/scaffold/ScaffoldDbContextDialogWrapper.kt
@@ -21,7 +21,6 @@ import com.jetbrains.observables.observableList
 import com.jetbrains.observables.ui.dsl.bindSelected
 import com.jetbrains.observables.ui.dsl.bindText
 import com.jetbrains.observables.ui.dsl.editableComboBox
-import com.jetbrains.rd.generator.nova.GenerationSpec.Companion.nullIfEmpty
 import com.jetbrains.rider.plugins.efcore.EfCoreUiBundle
 import com.jetbrains.rider.plugins.efcore.cli.api.models.DotnetEfVersion
 import com.jetbrains.rider.plugins.efcore.features.connections.DbConnectionInfo
@@ -105,7 +104,7 @@ internal class ScaffoldDbContextDialogWrapper(
             commonOptions,
             dataCtx.connection.value,
             dataCtx.provider.value,
-            dataCtx.outputFolder.value.nullIfEmpty(),
+            dataCtx.outputFolder.value.takeIf { it.isNotBlank() },
             dataCtx.useAttributes.value,
             dataCtx.useDatabaseNames.value,
             dataCtx.generateOnConfiguring.value,
